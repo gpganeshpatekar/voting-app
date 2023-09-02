a98b18349a09942cc946130bc26da9926252b8fb
gpganeshpatekar <gpganeshpatekar@gmail.com>
2023-09-02 13:37:27 +0530

    Bootstrap class impliment CLR for roles creation at the start up

--- src/main/java/com/votingapp/VotingAppApplication.java
+++ src/main/java/com/votingapp/VotingAppApplication.java
@@ -1,12 +1,23 @@
 package com.votingapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.votingapp.entities.Role;
+import com.votingapp.repositories.RoleRepository;
+
 @SpringBootApplication
-public class VotingAppApplication {
+public class VotingAppApplication implements CommandLineRunner {
+	
+	@Autowired
+	private RoleRepository roleRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VotingAppApplication.class, args);
@@ -16,5 +27,25 @@ public class VotingAppApplication {
     ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Role admin = new Role();
+		admin.setId(5001);
+		admin.setName("ROLE_ADMIN");
+		
+		Role normal = new Role();
+		normal.setId(5002);
+		normal.setName("ROLE_NORMAL");
+		
+		List<Role> roles = new ArrayList<>();
+		roles.add(admin);
+		roles.add(normal);
+		
+		roleRepo.saveAll(roles);
+		
+		
+	}
 
 }
